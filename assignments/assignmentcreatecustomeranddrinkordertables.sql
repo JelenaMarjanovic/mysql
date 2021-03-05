899ea5cd72deec474bb1df9ec55f544f84e5655a
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2021-03-05 17:05:32 +0100

    Create Customer and DrinkOrder tables

---
+++ assignments/assignmentcreatecustomeranddrinkordertables.sql
@@ -0,0 +1,23 @@
+-- DDL
+CREATE TABLE customer (
+  id INTEGER,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  address VARCHAR(100),
+  city VARCHAR(100),
+  state VARCHAR(3),
+  zip_code VARCHAR(5)
+);
+
+CREATE TABLE drink_order (
+  id INTEGER,
+  customer_id INTEGER,
+  drink_description VARCHAR(100)
+);
+
+-- DML
+INSERT INTO customer VALUES (1, 'Jelena', 'Marjanovic', 'Sarajevska 4', 'Beograd', 'SRB', '11000');
+INSERT INTO drink_order VALUES (1, 1, 'Single Malt Whisky');
+
+-- SELECT * FROM customer;
+SELECT * FROM drink_order;
