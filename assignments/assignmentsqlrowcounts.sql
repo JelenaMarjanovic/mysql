01ddad25f13b725191a2045ee5f570404067fac2
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2021-03-06 04:02:53 +0100

    Get Row Counts

---
+++ assignments/assignmentsqlrowcounts.sql
@@ -0,0 +1,13 @@
+use employees;
+-- What is the row count of the DEPARTMENTS table?
+SELECT count(*) FROM departments;
+-- What is the row count of the DEPT_EMP table?
+SELECT count(*) FROM dept_emp;
+-- What is the row count of the DEPT_MANAGER table?
+SELECT count(*) FROM dept_manager;
+-- What is the row count of the EMPLOYEES table?
+SELECT count(*) FROM employees;
+-- What is the row count of the SALARIES table?
+SELECT count(*) FROM salaries;
+-- What is the row count of the TITLES table?
+SELECT count(*) FROM titles;
