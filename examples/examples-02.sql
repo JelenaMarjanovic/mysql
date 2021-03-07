72311df572dac544de67174f4b760dd071cbf63d
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-03-07 04:20:17 +0100

    Selecting Specific Columns from Tables

---
+++ examples/examples-02.sql
@@ -0,0 +1,21 @@
+use employees;
+-- Select all from employees table
+SELECT 
+    *
+FROM
+    employees;
+-- Select specific 2 columns from employees table
+SELECT 
+    first_name, last_name
+FROM
+    employees;
+-- Select all from salaries table
+SELECT 
+    *
+FROM
+    salaries;
+-- Select specific 2 columns from salaries table
+SELECT 
+    emp_no, slary
+FROM
+    salaries;
