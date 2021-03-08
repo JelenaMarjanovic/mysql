904b5a7741afba99fde282ea906940a0c2f2bac5
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-03-08 02:23:59 +0100

    Formatting

--- examples/examples-04.sql
+++ examples/examples-04.sql
@@ -1,3 +1,8 @@
 use employees;
 -- Concatenate more fields together into a single value
-select first_name, last_name, concat(first_name, ' ', last_name) as 'Name' from employees;
+SELECT 
+    first_name,
+    last_name,
+    CONCAT(first_name, ' ', last_name) AS 'Name'
+FROM
+    employees;
